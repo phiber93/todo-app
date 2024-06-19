0f733fae20861749915b26f4db90a4243366851e
phiber93 <153130604+phiber93@users.noreply.github.com>
2024-06-19 11:18:34 +0200

    chapter 4, wip

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,14 +1,39 @@
 import { useParams, Link } from 'react-router-dom'
+import { useState } from 'react'
+import { retrieveHelloWorldPathVariable } from './api/HelloWorldApiService'
 
 export default function WelcomeComponent() {
 
     const { username } = useParams()
+
+    const [message, setMessage] = useState(null)
+
+    function callHelloWorldRestApi() {
+        retrieveHelloWorldPathVariable('Bernhard')
+            .then((response) => successfullResponse(response))
+            .catch((error) => errorResponse(error))
+            .finally(() => console.log("cleanup"))
+    }
+
+    function successfullResponse(response) {
+        console.log(response)
+        setMessage(response.data.message)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
+
     return (
         <div className="WelcomeComponent">
             <h1>Welcome {username} </h1>
             <div>
                 To Manage Your Todos: <Link to="/todos">Go here</Link>
             </div>
+            <div >
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>Call Hello World</button>
+            </div>
+            <div className="text-info">{message}</div>
         </div>
     )
 }
